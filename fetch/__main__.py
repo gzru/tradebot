d8985a1605e7760eeadc7e509153057d0c14aabd
Dmitry Vasilyev <gz_@bk.ru>
2021-08-15 18:04:39 +0000

    rm default arg values + move output fname fmt outside

--- fetch/__main__.py
+++ fetch/__main__.py
@@ -9,10 +9,10 @@ from structs.candle import Candle
 parser = argparse.ArgumentParser(description="Fetch candles from binance")
 
 # Add the arguments
-parser.add_argument("--symbol", type=str, default="ETHBTC")
-parser.add_argument("--interval", type=str, help="1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M", default="5m")
-parser.add_argument("-n", type=int, default=20000)
-parser.add_argument("--dir", type=str, default="data")
+parser.add_argument("--symbol", type=str, required=True)
+parser.add_argument("--interval", type=str, help="1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M", required=True)
+parser.add_argument("-n", type=int, required=True)
+parser.add_argument("--output", type=str, required=True)
 
 # Execute the parse_args() method
 args = parser.parse_args()
@@ -20,13 +20,11 @@ args = parser.parse_args()
 # Create binance connector
 client = Spot()
 
-output_fname = "{}/{}.{}.csv".format(args.dir, args.symbol, args.interval)
-
 count = fetch_last_candles_to_csv(client,
                                   symbol=args.symbol,
                                   interval=args.interval,
                                   limit=args.n,
-                                  output_fname=output_fname)
+                                  output_fname=args.output)
 
 print("Records fetched: {}".format(count))
 
