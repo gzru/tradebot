99fe502622ed6f397c67192b57a631818aee114e
Dmitry Vasilyev <gz_@bk.ru>
2021-08-15 18:09:18 +0000

    remove defaults + add CCAT target field

--- trainset/__main__.py
+++ trainset/__main__.py
@@ -1,17 +1,17 @@
 import argparse
 
 from collections import deque
-from structs.klines import FIELDS, make_field_list, filter_fields_csv
 from structs.candle import Candle, make_batch_name_list
+from trainset.transform import make_3cat_transform
 
 
 # Create the parser
 parser = argparse.ArgumentParser(description="Prepare csv dataset")
 
 # Add the arguments
-parser.add_argument("--data", type=str, default="data/ETHBTC.5m.csv")
-parser.add_argument("--output", type=str, default="data/trainset.csv")
-parser.add_argument("--predict-field", type=str, default="c")
+parser.add_argument("--data", type=str, required=True)
+parser.add_argument("--output", type=str, required=True)
+parser.add_argument("--predict-field", type=str, required=True)
 parser.add_argument("--train-batch", type=int, default=100)
 
 # Execute the parse_args() method
@@ -24,9 +24,16 @@ def write_header(predict_field: str, train_batch: int, fd):
     fd.write(", ".join(fields))
     fd.write("\n")
 
+def get_predict_field(predict_field: str, batch) -> str:
+    if predict_field == "CCAT":
+        return str(make_3cat_transform(float(batch[0].get_field("c")),
+                                       float(batch[1].get_field("c")),
+                                       0.01))
+    return batch[0].get_field(predict_field)
+
 def write_batch(predict_field: str, batch, fd):
     # Extract target field from the first candle
-    fd.write(batch[0].get_field(predict_field))
+    fd.write(get_predict_field(predict_field, batch))
     fd.write(", ")
     # Rest of the batch
     parts = list()
