e8904e897755314ca6a4f3b54e161f38e04fbace
Dmitry Vasilyev <gz_@bk.ru>
2021-08-15 18:08:18 +0000

    remove defaults + experiment with categorification

--- train/__main__.py
+++ train/__main__.py
@@ -8,10 +8,10 @@ from structs.candle import make_batch_name_list
 parser = argparse.ArgumentParser(description="Train a model")
 
 # Add the arguments
-parser.add_argument("--trainset", type=str, default="data/trainset.csv")
-parser.add_argument("--output", type=str, default="data/model.pkl")
-parser.add_argument("--predict-field", type=str, default="c")
-parser.add_argument("--train-batch", type=int, default=100)
+parser.add_argument("--trainset", type=str, required=True)
+parser.add_argument("--output", type=str, required=True)
+parser.add_argument("--predict-field", type=str, required=True)
+parser.add_argument("--train-batch", type=int, required=True)
 parser.add_argument("--train-cycles", type=int, default=30)
 
 # Execute the parse_args() method
@@ -22,10 +22,12 @@ field_names = make_batch_name_list(batch_size=args.train_batch)
 loader = TabularDataLoaders.from_csv(args.trainset,
                                      path="",
                                      y_names=args.predict_field,
-                                     cont_names=field_names)
+                                     cat_names=[args.predict_field],
+                                     cont_names=field_names,
+                                     proc=[Categorify])
 
-# learner = tabular_learner(loader, metrics=[rmse], layers=[300, 200, 100, 50, 20, 1])
-learner = tabular_learner(loader, metrics=[rmse])
+#learner = tabular_learner(loader, metrics=[accuracy, rmse], layers=[300, 200, 100, 50, 20, 1])
+learner = tabular_learner(loader, metrics=[accuracy])
 
 # Exploring the learning rates
 # learner.lr_find(start_lr = 1e-05,end_lr = 1e+05, num_it = 1000)
