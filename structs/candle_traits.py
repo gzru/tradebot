9d7258dcc34f7b67c548caa1383d58639b432fd0
Dmitry Vasilyev <gz_@bk.ru>
2021-09-16 09:52:53 +0000

    interface extensions

--- structs/candle_traits.py
+++ structs/candle_traits.py
@@ -1,4 +1,4 @@
-from typing import Iterable, Final, List, Mapping
+from typing import Iterable, Final, List, Mapping, Any
 
 
 class CandleTraits:
@@ -17,12 +17,17 @@ class CandleTraits:
         "i",
     ]
 
-    def __init__(self, fields: Iterable[str] = DEFAULT_FIELD_LIST):
-        self.fields: List[str]  = list(fields)
-        self.fields_count: int = len(self.fields)
-        self.fields_index: Mapping[str, int] = dict()
-        for i, field in enumerate(self.fields):
-            self.fields_index[field] = i
+    def __init__(self, fields: Iterable[str]):
+        self._set_fields(fields)
+
+    def join(self, other: Any) -> None:
+        self._set_fields(self.fields + other.fields)
+
+    def rename(self, prefix: str) -> None:
+        self._set_fields([prefix + field for field in self.fields])
+
+    def to_csv(self) -> str:
+        return ",".join(self.fields)
 
     def make_batch_fields(self, batch_size: int) -> List[str]:
         names_seq = list()
@@ -32,5 +37,17 @@ class CandleTraits:
         return names_seq
 
     @classmethod
+    def base(cls):
+        return cls(cls.DEFAULT_FIELD_LIST)
+
+    @classmethod
     def from_csv(cls, header: str):
         return cls(map(str.strip, header.strip().split(",")))
+
+    def _set_fields(self, fields: Iterable[str]) -> None:
+        self.fields: List[str]  = list(fields)
+        self.fields_count: int = len(self.fields)
+        self.fields_index: Mapping[str, int] = dict()
+        for i, field in enumerate(self.fields):
+            assert field not in self.fields_index
+            self.fields_index[field] = i
