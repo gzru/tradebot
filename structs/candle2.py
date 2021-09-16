6d0ab5702b1dd31550028ee82561a94243c47815
Dmitry Vasilyev <gz_@bk.ru>
2021-09-16 09:52:09 +0000

    extend interface and add converters: make_*

--- structs/candle2.py
+++ structs/candle2.py
@@ -1,5 +1,8 @@
-from typing import Sequence, Any
+import pandas
+import torch
+from typing import Sequence, Any, TextIO, List
 from torch import Tensor, zeros
+from numpy import ndarray, float32, char, savetxt
 from structs.candle_traits import CandleTraits
 
 
@@ -14,7 +17,7 @@ class Candle:
         self.data[traits.fields_index[name]] = value
 
     def to_csv(self) -> str:
-        return ", ".join(map(str, map(Tensor.item, self.data)))
+        return ",".join(map(str, map(Tensor.item, self.data)))
 
     def copy_from(self, candle: Any) -> None:
         self.data.copy_(candle.data)
@@ -26,6 +29,13 @@ class Candle:
     def denormalize(self, offsets: Tensor, dividers: Tensor) -> None:
         self.data *= dividers
         self.data += offsets
+
+    def convert(self, source_traits: CandleTraits, dest_traits: CandleTraits):
+        data = zeros(dest_traits.fields_count)
+        for i, field in enumerate(dest_traits.fields):
+            j = source_traits.fields_index[field]
+            data[i] = self.data[j]
+        return Candle(data)
 
     @classmethod
     def plain(cls, traits: CandleTraits):
@@ -41,6 +51,24 @@ class Candle:
             tensor[i] = value
         return cls(tensor)
 
+    @classmethod
+    def from_numpy(cls, data: ndarray, traits: CandleTraits):
+        assert traits.fields_count == len(data)
+        assert data.dtype == float32
+        return cls(Tensor(data))
+
     @staticmethod
     def from_csv(record: str, traits: CandleTraits):
-        return Candle.from_data(record.strip().split(", "), traits)
+        return Candle.from_data(record.strip().split(","), traits)
+
+
+def make_copy(candles: Sequence[Candle]) -> List[Candle]:
+    return [Candle(candle.data.clone()) for candle in candles]
+
+def make_pandas_series(candles: Sequence[Candle], names: Sequence[str]) -> pandas.Series:
+    data = torch.cat([candle.data for candle in candles])
+    return pandas.Series(data=data, index=names)
+
+def make_pandas_data_frame(candles: Sequence[Candle], traits: CandleTraits) -> pandas.DataFrame:
+    data = torch.stack([candle.data for candle in candles])
+    return pandas.DataFrame(data=data, columns=traits.fields)
