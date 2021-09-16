5024dcc814c69ccc0dcccdcef6d2a46453a6772e
Dmitry Vasilyev <gz_@bk.ru>
2021-09-16 09:50:35 +0000

    implement std normalizer using torch capabilities

--- structs/normalization.py
+++ structs/normalization.py
@@ -1,13 +1,14 @@
+import torch
+
 from typing import Sequence, List, Optional
-from torch import Tensor, zeros, ones, maximum, minimum
 from structs.candle2 import Candle
 from structs.candle_traits import CandleTraits
 
 
 class Normalizer:
-    def __init__(self, traits: CandleTraits, offsets: Tensor, dividers: Tensor):
-        self.offsets: Tensor = offsets
-        self.dividers: Tensor = dividers
+    def __init__(self, traits: CandleTraits, offsets: torch.Tensor, dividers: torch.Tensor):
+        self.offsets: torch.Tensor = offsets
+        self.dividers: torch.Tensor = dividers
         self.candle: Candle = Candle.plain(traits)
 
     def apply(self, candle: Candle) -> None:
@@ -30,8 +31,8 @@ class Normalizer:
 class PlainNormalizerFactory:
     def __init__(self, traits: CandleTraits):
         self.traits: CandleTraits = traits
-        self.offsets: Tensor = zeros(traits.fields_count)
-        self.dividers: Tensor = ones(traits.fields_count)
+        self.offsets: torch.Tensor = torch.zeros(traits.fields_count)
+        self.dividers: torch.Tensor = torch.ones(traits.fields_count)
 
     def make_normalizer(self, candles: Sequence[Candle]):
         return Normalizer(self.traits, self.offsets, self.dividers)
@@ -39,17 +40,17 @@ class PlainNormalizerFactory:
 class MinMaxNormalizerFactory:
     def __init__(self, traits: CandleTraits):
         self.traits: CandleTraits = traits
-        self.min_values: Tensor = zeros(traits.fields_count)
-        self.max_values: Tensor = zeros(traits.fields_count)
-        self.dividers: Tensor = zeros(traits.fields_count)
+        self.min_values: torch.Tensor = torch.zeros(traits.fields_count)
+        self.max_values: torch.Tensor = torch.zeros(traits.fields_count)
+        self.dividers: torch.Tensor = torch.zeros(traits.fields_count)
 
     def make_normalizer(self, candles: Sequence[Candle]):
         self.min_values.copy_(candles[0].data)
         self.max_values.copy_(candles[0].data)
         self.dividers.fill_(1.0)
         for j in range(1, len(candles)):
-            minimum(self.min_values, candles[j].data, out=self.min_values)
-            maximum(self.max_values, candles[j].data, out=self.max_values)
+            torch.minimum(self.min_values, candles[j].data, out=self.min_values)
+            torch.maximum(self.max_values, candles[j].data, out=self.max_values)
         for i in range(self.traits.fields_count):
             if self.min_values[i] != self.max_values[i]:
                 self.dividers[i] = self.max_values[i] - self.min_values[i]
@@ -58,47 +59,19 @@ class MinMaxNormalizerFactory:
 class StdNormalizerFactory:
     def __init__(self, traits: CandleTraits):
         self.traits: CandleTraits = traits
-        self.mean_values: Tensor = zeros(traits.fields_count)
-        self.deviations: Tensor = zeros(traits.fields_count)
-        self.temp: Tensor = zeros(traits.fields_count)
+        self.mean_values: torch.Tensor = torch.zeros(traits.fields_count)
+        self.deviations: torch.Tensor = torch.zeros(traits.fields_count)
+        self.temp: torch.Tensor = torch.zeros(traits.fields_count)
 
     def make_normalizer(self, candles: Sequence[Candle]):
-        self.mean_values.fill_(0.0)
-        self.deviations.fill_(0.0)
-        """
-        for i in range(NUM_FIELDS):
-            self.mean_values[i] = 0.0
-            self.deviations[i] = 0.0
-        """
-        for candle in candles:
-            self.mean_values.add_(candle.data)
-            """
-            for i, value in enumerate(candle.get_data_all()):
-                self.mean_values[i] += value
-            """
-        self.mean_values.div_(len(candles))
-        """
-        for i in range(NUM_FIELDS):
-            self.mean_values[i] /= len(candles)
-        """
-        for candle in candles:
-            self.temp.copy_(candle.data)
-            self.temp.sub_(self.mean_values)
-            self.temp.mul_(self.temp)
-            self.deviations.add_(self.temp)
-            """
-            for i, value in enumerate(candle.get_data_all()):
-                deviation = value - self.mean_values[i]
-                self.deviations[i] +=  deviation * deviation
-            """
+        temp = torch.stack([candle.data for candle in candles])
+        self.deviations, self.mean_values = torch.std_mean(temp, 0, unbiased=False)
         for i in range(self.traits.fields_count):
-            if self.deviations[i] != 0.0:
-                self.deviations[i] = (self.deviations[i] / len(candles)) ** 0.5
-            else:
+            if self.deviations[i] == 0.0:
                 self.deviations[i] = 1.0
         return Normalizer(self.traits, self.mean_values, self.deviations)
 
-def make_normalizer_factory(traits: CandleTraits, normalization: Optional[str]):
+def make_normalizer_factory(normalization: Optional[str], traits: CandleTraits):
     if not normalization:
         return PlainNormalizerFactory(traits)
     normalization = normalization.lower()
